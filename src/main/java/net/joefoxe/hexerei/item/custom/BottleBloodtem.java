0d90900065642b2e42e1e7b14f0c0794831be0e5
Harineko0 <harineko0927@gmail.com>
2024-03-01 22:22:44 +0900

    fix: remove import IItemRenderProperties.

--- src/main/java/net/joefoxe/hexerei/item/custom/BottleBloodtem.java
+++ src/main/java/net/joefoxe/hexerei/item/custom/BottleBloodtem.java
@@ -17,7 +17,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.IItemRenderProperties;
 
 import javax.annotation.Nullable;
 import java.util.List;
